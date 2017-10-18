8a28714e6ed894e94211d573f882ca8deb2abc76
saikrishnamns <32727280+saikrishnamns@users.noreply.github.com>
2017-10-18 15:28:59 +0200

    class
    classroom

---
+++ src/main/java/io/rscale/training/company/CloudDataConfig.java
@@ -0,0 +1,50 @@
+
+package io.rscale.training.company;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.UnsatisfiedDependencyException;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("cloud")
+@Configuration
+public class CloudDataConfig extends AbstractCloudConfig {
+
+	private static final Logger logger = Logger.getLogger(CloudDataConfig.class);
+
+	public CloudDataConfig() {
+		logger.info(this.getClass() + " loaded");
+	}
+
+	@Bean
+	public DataSource dataSource() throws Exception {
+		DataSource dataSource = cloud().getSingletonServiceConnector(DataSource.class, null);
+		if (!isMySQL(dataSource)) {
+			logger.error("MySQL required when running cloud profile.");
+			throw new UnsatisfiedDependencyException("javax.sql.DataSource", "javax.sql.DataSource",
+					"javax.sql.DataSource", "MySQL required when running cloud profile.");
+		}
+		return dataSource;
+	}
+
+	private boolean isMySQL(DataSource dataSource) {
+		// implement me!
+		try {
+			logger.info("product name"+dataSource.getConnection().getMetaData().getDatabaseProductName());
+			if (dataSource.getConnection().getMetaData().getDatabaseProductName().contains("MySQL")) {
+				return true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			logger.error("failed");
+			return false;
+		}
+		return false;
+	}
+}
--- src/main/java/io/rscale/training/company/DataSourceConfig.java
+++ src/main/java/io/rscale/training/company/DataSourceConfig.java
@@ -1,15 +1,17 @@
 package io.rscale.training.company;
 
 import javax.annotation.PostConstruct;
+
 import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
-
+@Profile("local")
 @Configuration
 public class DataSourceConfig {
 
